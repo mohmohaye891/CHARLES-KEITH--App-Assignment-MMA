489cd693e10dcc7e2042aecb280321a6e98c4daf
Unknown <mohmohaye8891@gmail.com>
2018-06-30 12:21:22 +0630

    Update MainActivity

--- app/src/main/java/com/padcmyanmar/charles_keith__app_assignment_mma/activities/MainActivity.java
+++ app/src/main/java/com/padcmyanmar/charles_keith__app_assignment_mma/activities/MainActivity.java
@@ -3,10 +3,13 @@ package com.padcmyanmar.charles_keith__app_assignment_mma.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.padcmyanmar.charles_keith__app_assignment_mma.R;
 import com.padcmyanmar.charles_keith__app_assignment_mma.adapters.NewProductsAdapter;
@@ -19,23 +22,48 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity implements NewProductsDelegate{
 
     private NewProductsAdapter mNewProductAdapter;
+
+    @BindView(R.id.iv_view1)
+    ImageView ivViewOne;
+
+    @BindView(R.id.iv_view2)
+    ImageView ivViewTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this, this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        RecyclerView rvNewProduct = findViewById(R.id.rv_charlie_view1);
+        final RecyclerView rvNewProduct = findViewById(R.id.rv_charlie_view1);
         mNewProductAdapter = new NewProductsAdapter(this);
         rvNewProduct.setAdapter(mNewProductAdapter);
         rvNewProduct.setLayoutManager(new LinearLayoutManager(getApplicationContext(),
                 LinearLayoutManager.VERTICAL, false));
+
+        ivViewOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rvNewProduct.setLayoutManager(new LinearLayoutManager(getApplicationContext(),
+                        LinearLayoutManager.VERTICAL, false));
+            }
+        });
+
+        ivViewTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rvNewProduct.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+            }
+        });
 
         NewProductModel.getObjInstance().LoadNewProducts();
     }
